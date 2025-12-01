e932e52b091daf5a9700f5a461eddd322c4486f5
Tanjinatisha <tanjinatisha53@gmail.com>
2025-12-01 23:50:16 +0600

    Add files via upload

---
+++ 070_name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+int main() {
+int num;
+cout << "Enter a decimal number: ";
+cin >> num;
+stringstream ss;
+ss << oct << num;
+cout << "Octal: " << ss.str() << endl;
+return 0;
+}
---
+++ 072_name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+int main() {
+int num;
+cout << "Enter a decimal number: ";
+cin >> num;
+stringstream ss;
+ss << hex << num;
+cout << "Hexadecimal: " << ss.str() << endl;
+return 0;
+}
