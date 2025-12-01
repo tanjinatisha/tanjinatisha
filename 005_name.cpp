35fe8bc56bfedc9ad51c7e04c510440ed6ffd533
Tanjinatisha <tanjinatisha53@gmail.com>
2025-12-01 23:13:45 +0600

    Add files via upload

---
+++ 005_name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+float a, b;
+cout << "Enter first number: ";
+cin >> a;
+cout << "Enter second number: ";
+cin >> b;
+float product = a * b;
+cout << "Product: " << product << endl;
+return 0;
+}
