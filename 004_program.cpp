6fd47ac206985099f774f6185b9a1b75273d45cc
Tanjinatisha <tanjinatisha53@gmail.com>
2025-12-01 23:10:25 +0600

    Add files via upload

---
+++ 004_program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a, b;
+cout << "Enter first number: ";
+cin >> a;
+cout << "Enter second number: ";
+cin >> b;
+int sum = a + b;
+cout << "Sum: " << sum << endl;
+return 0;
+}
