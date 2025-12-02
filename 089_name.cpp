5d0819233a4ab4fceaeacf61a6271f2c994da77e
Tanjinatisha <tanjinatisha53@gmail.com>
2025-12-02 11:43:21 +0600

    Add files via upload

---
+++ 089_name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main() {
+const char* oldName = "oldfile.txt";
+const char* newName = "newfile.txt";
+if(rename(oldName, newName) == 0)
+cout << "File renamed successfully." << endl;
+else
+perror("Error renaming file");
+return 0;
+}
